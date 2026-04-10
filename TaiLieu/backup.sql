82b838ad851d2cbbde9c9e6d9bf3861fb3c66e8f
Huỳnh Ngọc Phú <phuhuynhphoto2k4@gmail.com>
2026-04-10 14:52:59 +0700

    Delete TaiLieu

--- TaiLieu/backup.sql
+++
@@ -1,5 +0,0 @@
-BACKUP DATABASE [CAFEBOOKDB_v2]
-TO DISK = N'E:\Tai Lieu Hoc Tap\CS 445 AM\Project\TaiLieu\code\CAFEBOOKDB_v2.bak'
-WITH FORMAT,
-     INIT,
-     NAME = N'Backup_Full_CAFEBOOKDB_v2';
